4404f175d66f407bad2f4ea2c4ecb09aa2b7500d
takgeun-O <takgeun92@gmail.com>
2025-08-22 18:38:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long a, b, c;
+
+        a = scanner.nextLong();
+        b = scanner.nextLong();
+        c = scanner.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
