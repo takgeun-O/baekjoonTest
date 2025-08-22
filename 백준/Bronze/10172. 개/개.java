65a00c2a7190a7aa51283daa766f9c4a24472967
takgeun-O <takgeun92@gmail.com>
2025-08-22 18:42:47 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+//|q p|   /}
+//        ( 0 )"""\
+//|"^"`    |
+//||_/=\\__|
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
