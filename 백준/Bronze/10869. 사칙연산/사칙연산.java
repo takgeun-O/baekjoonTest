3e4059f830fefc5ab85e9319a8201a7cbf5ada42
takgeun-O <takgeun92@gmail.com>
2025-08-22 17:53:51 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
