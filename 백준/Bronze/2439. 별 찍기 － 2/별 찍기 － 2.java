7a6ba4f4cf0dfa87d71c6affc12c2f70b377b6d2
takgeun-O <takgeun92@gmail.com>
2025-08-25 20:47:48 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 284 ms, Memory: 18504 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N-i-1;j++) {
+                System.out.print(" ");
+            }
+            for(int j=0;j<i+1;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
