04fa8b40157abf51c45d2fb5c1c60d817008ed39
takgeun-O <takgeun92@gmail.com>
2025-09-12 05:01:13 +0900

    [Bronze V] Title: 문자열, Time: 96 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -2,19 +2,30 @@
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        scanner.nextLine();
+        InputStream inputStream = System.in;
+        InputStreamReader sr = new InputStreamReader(inputStream);
+        BufferedReader br = new BufferedReader(sr);
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
         for(int i=0;i<T;i++) {
-            String st = scanner.nextLine();
-            System.out.println(st.charAt(0) + "" + st.charAt(st.length()-1));
+            String s = br.readLine();
+
+            char first = s.charAt(0);
+            char last = s.charAt(s.length()-1);
+
+            sb.append(first).append(last).append("\n");
         }
+
+        System.out.println(sb.toString());
     }
 }
