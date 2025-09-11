890fd668d82640d4f8c6d333b8b851e1da10813f
takgeun-O <takgeun92@gmail.com>
2025-09-12 04:25:43 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,17 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        System.out.println(input.length());
+    }
+}
