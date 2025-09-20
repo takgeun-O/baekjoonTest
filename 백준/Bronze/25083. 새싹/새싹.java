5219988f996b06f2ec407e717d616b1b8bf55a24
takgeun-O <takgeun92@gmail.com>
2025-09-20 10:32:37 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
