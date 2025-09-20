da6e964e0570b5ab12bad9ed6aea0c336f31acad
takgeun-O <takgeun92@gmail.com>
2025-09-20 09:28:14 +0900

    [Bronze II] Title: 단어의 개수, Time: 288 ms, Memory: 52764 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -9,18 +9,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
 
-        String line = scanner.nextLine().trim();
-        scanner.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        if(line.isEmpty()) {
+        String input = br.readLine().trim();
+
+        if(input.isEmpty()) {
             System.out.println(0);
         } else {
-            String[] words = line.split(" ");
+            String[] words = input.split(" ");
             System.out.println(words.length);
         }
-
 
     }
 }
