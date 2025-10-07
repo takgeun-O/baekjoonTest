6a2aeb41f30f3262cd4de2fffb5ea4650447cf9d
takgeun-O <takgeun92@gmail.com>
2025-10-07 23:35:38 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -7,33 +7,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        char[][] matrix = new char[5][15];
+        String[] lines = new String[5];
+        int[] len = new int[5];
+        int maxLen = 0;
 
         for(int i=0;i<5;i++) {
-            String input = reader.readLine();
-            for(int j=0;j<input.length();j++) {
-                matrix[i][j] = input.charAt(j);
-            }
-            for(int j=input.length();j<15;j++) {
-                matrix[i][j] = ' ';
+            lines[i] = reader.readLine();
+            len[i] = lines[i].length();
+            if(maxLen < len[i]) {
+                maxLen = len[i];
             }
         }
 
-//        for(int i=0;i<5;i++) {
-//            for(int j=0;j<15;j++) {
-//                System.out.print(matrix[i][j]);
-//            }
-//            System.out.println();
-//        }
-
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<15;i++) {
-            for(int j=0;j<5;j++) {
-                if(matrix[j][i] == ' ') {
-                    continue;
+        for(int col=0;col<maxLen;col++) {
+            for(int row=0;row<5;row++) {
+                if(col < lines[row].length()) {
+                    sb.append(lines[row].charAt(col));
                 }
-                sb.append(matrix[j][i]);
             }
         }
 
