7fdccf0330afa25a9f1fed7a6111fa6d11185198
takgeun-O <takgeun92@gmail.com>
2025-12-09 08:05:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.03 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 32.5 MB, 시간: 46.29 ms
+메모리: 97.1 MB, 시간: 69.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 09일 07:40:15
+2025년 12월 09일 08:05:53
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,21 +1,26 @@
 import java.util.*;
 import java.io.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         
         HashMap<String, Integer> hashMap = new HashMap<>();
         
-        for(String string : completion) {
-            hashMap.put(string, hashMap.getOrDefault(string, 0) + 1);
+        for(String s : completion) {
+            hashMap.put(s, hashMap.getOrDefault(s, 0) + 1);
         }
         
-        for(String string : participant) {
-            if(hashMap.getOrDefault(string, 0) == 0) {
-                return string;
+        // for(Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+        //     System.out.println(entry.getKey() + " = " + entry.getValue());
+        // }
+        
+        for(String s : participant) {
+            if(hashMap.getOrDefault(s, 0) == 0) {
+                return s;
             }
-            hashMap.put(string, hashMap.get(string) - 1);
+            hashMap.put(s, hashMap.getOrDefault(s, 0) - 1);
         }
         
-        return null;
+        return "";
     }
 }
