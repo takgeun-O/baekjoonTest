3b82662aac78be99c3e36d0bb8002a948c771483
takgeun-O <takgeun92@gmail.com>
2025-12-23 23:54:04 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        
+        for(answer=0;a!=b;answer++) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+        }
+        
+        return answer;
+    }
+}
