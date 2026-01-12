936bdeffb3aa27c1cc9969861ec06b215ca2a4d6
takgeun-O <takgeun92@gmail.com>
2026-01-12 22:02:24 +0900

    [level 2] Title: 미로 탈출, Time: 7.81 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,27 +1,29 @@
 import java.util.*;
+
 class Solution {
     
+    private static char[][] grid;
     // 오른쪽, 아래, 왼쪽, 위
     private static final int[] dr = {0, 1, 0, -1};
     private static final int[] dc = {1, 0, -1, 0};
-    private static char[][] grid;
-    private static int n, m;
+    private static int n;
+    private static int m;
     
     private static class Node {
-        int r;
-        int c;
+        int r, c;
+        
         public Node(int r, int c) {
             this.r = r;
             this.c = c;
         }
     }
     
-    private static int bfs(Node start, Node end) {
-        int[][] dist = new int[n][m];
+    private static int bfs(Node from, Node to) {
         ArrayDeque<Node> queue = new ArrayDeque<>();
+        int[][] dist = new int[n][m];
         
-        dist[start.r][start.c] = 1;
-        queue.addLast(start);
+        dist[from.r][from.c] = 1;
+        queue.addLast(from);
         
         while(!queue.isEmpty()) {
             Node now = queue.pollFirst();
@@ -30,37 +32,38 @@ class Solution {
                 int nr = now.r + dr[i];
                 int nc = now.c + dc[i];
                 
-                // 맵 밖으로 벗어나는 예외 처리
+                // System.out.println(nr + " " + nc);
+                
+                // 맵 바깥으로 벗어날 때
                 if(nr < 0 || nc < 0 || nr >= n || nc >= m) {
                     continue;
                 }
-                // 해당 칸이 벽일 경우 예외 처리
+                // 벽으로 막혔을 때
                 if(grid[nr][nc] == 'X') {
                     continue;
                 }
-                // 이미 방문했을 경우 예외 처리
-                if(dist[nr][nc] > 0) {
-                    continue;
+                
+                // 방문 안했을 때만 큐에 넣고 갱신
+                if(dist[nr][nc] == 0) {
+                    dist[nr][nc] = dist[now.r][now.c] + 1;
+                    queue.addLast(new Node(nr, nc));
                 }
                 
-                // 우선 걸린시간 카운트하면서 방문 처리해놓고
-                dist[nr][nc] = dist[now.r][now.c] + 1;
-                // 만약 해당 dist 위치의 값이 end 위치와 동일하면 곧바로 걸린 시간 리턴
-                if(nr == end.r && nc == end.c) {
+                // 목표 지점 도달 시 곧바로 반환
+                if(nr == to.r && nc == to.c) {
                     return dist[nr][nc] - 1;
                 }
-                queue.addLast(new Node(nr, nc));
             }
         }
         return -1;
     }
+    
     
     public int solution(String[] maps) {
         
         n = maps.length;
         m = maps[0].length();
         
-        // 주어진 맵을 grid 로 구현
         grid = new char[n][m];
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
@@ -68,42 +71,30 @@ class Solution {
             }
         }
         
-        // for(int i=0;i<n;i++) {
-        //     for(int j=0;j<m;j++) {
-        //         System.out.print(grid[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        
-        // 시작지점, 레버지점, 출구지점 찾기
         Node start = null;
         Node lever = null;
         Node end = null;
-        for(int r=0;r<n;r++) {
-            for(int c=0;c<m;c++) {
-                if(grid[r][c]=='S') {
-                    start = new Node(r, c);
-                } else if(grid[r][c]=='L') {
-                    lever = new Node(r, c);
-                } else if(grid[r][c]=='E') {
-                    end = new Node(r, c);
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(grid[i][j] == 'S') {
+                    start = new Node(i, j);
+                } else if(grid[i][j] == 'L') {
+                    lever = new Node(i, j);
+                } else if(grid[i][j] == 'E') {
+                    end = new Node(i, j);
                 }
             }
         }
         
-        // System.out.println(start.r + " " + start.c);
-        // System.out.println(lever.r + " " + lever.c);
-        // System.out.println(end.r + " " + end.c);
+        // System.out.println("start -> (" + start.r + ", " + start.c + ")");
+        // System.out.println("lever -> (" + lever.r + ", " + lever.c + ")");
+        // System.out.println("end -> (" + end.r + ", " + end.c + ")");
         
-        // 시작지점부터 레버지점까지의 최단거리를 bfs로 구하기
         int startToLever = bfs(start, lever);
-        // 레버지점부터 출구지점까지 최단거리를 bfs로 구하기
         int leverToEnd = bfs(lever, end);
         
-        if(startToLever == -1) {
-            return -1;
-        }
-        if(leverToEnd == -1) {
+        if(startToLever == -1 || leverToEnd == -1) {
             return -1;
         }
         
