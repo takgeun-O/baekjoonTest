267b7014fe9f6c1958d6d04eef58cd11d505002a
takgeun-O <takgeun92@gmail.com>
2026-01-19 19:58:29 +0900

    [level 2] Title: 피로도, Time: 2.91 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,10 +1,12 @@
+import java.util.*;
 class Solution {
     
+    private static boolean[] visited;
     private static int answer;
     private static int[][] Dungeons;
-    private static boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
+        
         answer = 0;
         Dungeons = dungeons;
         
@@ -15,17 +17,14 @@ class Solution {
         return answer;
     }
     
-    private static void backtrack(int k, int cnt) {
+    private static void backtrack(int k, int count) {
+        
         for(int i=0;i<Dungeons.length;i++) {
-            // 현재 피로도가 i번째 던전의 최소 필요 피로도보다 크거나 같고
-            // i번째 던전을 방문한 적이 없다면
+            
             if(!visited[i] && k >= Dungeons[i][0]) {
                 visited[i] = true;
-                
-                // 현재까지의 최대 탐험 가능 던전 수와
-                // i번째 던전에서 이동할 수 있는 최대 탐험 가능 던전 수 중 큰 값을 선택해서 업데이트
-                backtrack(k - Dungeons[i][1], cnt + 1);
-                answer = Math.max(answer, cnt + 1);
+                backtrack(k - Dungeons[i][1], count + 1);
+                answer = Math.max(answer, count + 1);
                 visited[i] = false;
             }
         }
