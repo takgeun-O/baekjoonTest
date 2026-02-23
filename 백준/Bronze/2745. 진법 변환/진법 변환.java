b53ec06ef692c1592b68c8f6239cefc34b7c061a
takgeun-O <takgeun92@gmail.com>
2026-02-23 19:43:40 +0900

    [Bronze II] Title: 진법 변환, Time: 96 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,60 +1,36 @@
-
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-import static java.lang.Math.abs;
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = reader.readLine();
+        String input = br.readLine();
 
-        StringTokenizer st = new StringTokenizer(input);
+        String[] splitInput = input.split("\\s+");
 
-        String N = st.nextToken();
-        int length = N.length();
-        int B = Integer.parseInt(st.nextToken());
+        String N = splitInput[0];
+        int B = Integer.parseInt(splitInput[1]);
 
-        // ABCDEFGHIJKLMNOPQRSTUVWXYZ 총 26개
-        // ZZZZZ 36
-        // A 는 10, B는 11, ... Z는 35
-        // 35*36^4 + 35*36^3 + 35*36^2 + 35*36^1 + 35*36^0
+        int result = 0;
+        for (int i = 0; i < N.length(); i++) {
 
-        Map<String, Integer> map = new HashMap<>();
+            char c = N.charAt(i);
+            int value = 0;
 
-//        String x = String.valueOf((char)('0' + 1));
-//        System.out.println(x);
-        for(int i=0;i<=9;i++) {
-            String x = String.valueOf((char)('0' + i));
-            map.put(x,i);
-        }
-
-        for(int i=0;i<26;i++) {
-            String x = String.valueOf((char)('A' + i));
-            map.put(x, i+10);
-        }
-
-//        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
-//            System.out.println(stringIntegerEntry);
-//        }
-
-        Integer total = 0;
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<length;i++) {
-            Integer digit = map.get(String.valueOf(N.charAt(i)));
-            Integer result = digit;
-            for(int j=0;j<length-1-i;j++) {
-                result = result * B;
+            if (c >= '0' && c <= '9') {
+                value = c - '0';
+            } else {
+                value = c - 'A' + 10;
             }
-//            System.out.println(result);
-            total = total + result;
+
+            result = result * B + value;
         }
 
-        System.out.println(total);
+        System.out.print(result);
     }
 }
-
-
