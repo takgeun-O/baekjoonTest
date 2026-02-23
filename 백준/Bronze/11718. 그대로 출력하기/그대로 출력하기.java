fcfa863fc7d648eb736657a340896dc599d8ce73
takgeun-O <takgeun92@gmail.com>
2026-02-23 19:24:23 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -1,22 +1,21 @@
-
-
-import java.io.*;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         String line;
-        // EOF까지
-        while((line=br.readLine()) != null) {
+        while ((line = br.readLine()) != null) {
             sb.append(line).append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.print(sb);
     }
 }
