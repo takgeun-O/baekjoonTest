11d2c4cf5b1b3f96fed176f2a1f410cb8b7979d7
takgeun-O <takgeun92@gmail.com>
2026-02-23 23:10:24 +0900

    [level 0] Title: 옹알이 (1), Time: 6.25 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,35 +3,9 @@ import java.io.*;
 class Solution {
     public int solution(String[] babbling) {
         
-        String[] arrString = {"aya", "ye", "woo", "ma"};
-        
-        int count = 0;
-        int flag = 0;
-        String check = "";
-        for(int i=0;i<babbling.length;i++) {
-            // System.out.println("-----------------------------------");
-            StringBuilder sb = new StringBuilder();
-            for(int j=0;j<babbling[i].length();j++) {
-                sb.append(babbling[i].charAt(j));
-                // System.out.println(sb + "*");
-                
-                check = sb.toString();
-                for(int k=0;k<arrString.length;k++) {
-                    if(check.equals(arrString[k])) {
-                        // System.out.println(check + " --> 일치");
-                        flag = 1;
-                        sb = new StringBuilder();
-                    }
-                }
-            }
-            
-            // System.out.println(sb);
-            if(sb.length() == 0 && flag == 1) {
-                count++;
-            }
-            flag = 0;
-        }
-        
-        return count;
+        return (int) Arrays.stream(babbling)
+                           .map(s -> s.replaceAll("(aya|ye|woo|ma)", ""))
+                           .filter(s -> s.equals(""))
+                           .count();
     }
 }
