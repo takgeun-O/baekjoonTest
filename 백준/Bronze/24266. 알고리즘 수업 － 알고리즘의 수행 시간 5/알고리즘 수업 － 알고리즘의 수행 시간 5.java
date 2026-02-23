162faf95c963cdf6f6937f932af260b8f96935a2
takgeun-O <takgeun92@gmail.com>
2026-02-23 21:32:19 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}
