aa5e240c9daa68ac4fc7882ac195faf0fc481d96
takgeun-O <takgeun92@gmail.com>
2026-02-23 20:08:22 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,40 +1,37 @@
-
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-import static java.lang.Math.abs;
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        // 10진법 수 N이 주어진다. 이 수를 B진법으로 바꿔 출력하는 프로그램을 작성하시오.
-        // 첫 줄에 N과 B가 주어진다.
-        // 첫째 줄에 10진법 수 N을 B진법으로 출력한다.
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-
-        // 60466175 36
-        // ZZZZZ
-
-        if (N == 0) {
-            System.out.println(0);
-            return;
-        }
-
         StringBuilder sb = new StringBuilder();
-        while (N > 0) {
-            int r = N % B;
-            char c = r < 10 ? (char)('0'+r) : (char) ('A'+(r-10));
-            sb.append(c);
+
+        String input = br.readLine();
+
+        String[] inputArr = input.split("\\s+");
+
+        int N = Integer.parseInt(inputArr[0]);
+        int B = Integer.parseInt(inputArr[1]);
+
+        String result;
+
+        while(N != 0) {
+
+            int value = N % B;
+
+            if(value >= 0 && value <= 9) {
+                sb.append((char)('0' + value));
+            } else {
+                sb.append((char)('A' + (value - 10)));
+            }
+
             N = N / B;
         }
 
         System.out.println(sb.reverse());
     }
 }
-
-
