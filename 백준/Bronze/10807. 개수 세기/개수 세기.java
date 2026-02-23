567db4c256be5072cd82b0903f790d65ed165c8d
takgeun-O <takgeun92@gmail.com>
2026-02-23 22:10:52 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,22 +1,27 @@
-
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int N = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
 
-        int[] input = new int[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        String arrString = br.readLine();
+        String[] line = arrString.split("\\s+");
         for(int i=0;i<N;i++) {
-            input[i] = scanner.nextInt();
+            arr[i] = Integer.parseInt(line[i]);
         }
+        int v = Integer.parseInt(br.readLine());
 
-        int v = scanner.nextInt();
         int count = 0;
         for(int i=0;i<N;i++) {
-            if(input[i]==v) {
+            if(arr[i] == v) {
                 count++;
             }
         }
