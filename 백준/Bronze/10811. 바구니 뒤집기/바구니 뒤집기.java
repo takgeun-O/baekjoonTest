792c7722165dcc305136822d72ae7edad0fd55ac
takgeun-O <takgeun92@gmail.com>
2026-02-25 18:47:52 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,50 +1,44 @@
-
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
 
-        int[] basket = new int[N];
-        for(int idx=0;idx<N;idx++) {
-            basket[idx] = idx+1;                // 바구니 번호 설정
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] bucket = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            bucket[i] = i;
         }
 
-        int i,j;
-        for(int count=0;count<M;count++) {
-            i = scanner.nextInt();
-            j = scanner.nextInt();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            int[] tempArr = new int[j-i+1];
-
-            for(int tempIdx=0;tempIdx<tempArr.length;tempIdx++) {           // 임시 배열에 옮기기
-                tempArr[tempIdx] = basket[j-1-tempIdx];
+            for (int j = 0; j <= (b - a) / 2; j++) {
+                int temp = bucket[a + j];
+                bucket[a + j] = bucket[b - j];
+                bucket[b - j] = temp;
             }
-
-//            for(int x=0;x< tempArr.length;x++) {
-//                System.out.print(tempArr[x] + " ");
-//            }
-//            System.out.println();
-
-            int tempIdx = 0;
-            for(int idx=i-1;idx<=j-1;idx++) {
-                basket[idx] = tempArr[tempIdx++];
-            }
-
-//            for(int x=0;x< basket.length;x++) {
-//                System.out.print(basket[x] + " ");
+//            for (int j = 1; j <= N; j++) {
+//                System.out.print(bucket[j] + " ");
 //            }
 //            System.out.println();
         }
 
-        for(int x=0;x< basket.length;x++) {
-            System.out.print(basket[x] + " ");
+        for (int i = 1; i <= N; i++) {
+            sb.append(bucket[i]).append(" ");
         }
 
-
+        System.out.println(sb);
     }
 }
