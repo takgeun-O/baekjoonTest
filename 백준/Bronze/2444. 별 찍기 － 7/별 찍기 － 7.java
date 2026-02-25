e8323c5fe88a60022344ab8a5b338f7f580cd26b
takgeun-O <takgeun92@gmail.com>
2026-02-25 14:43:53 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 116 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,38 +1,37 @@
-
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
 
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N-i-1;j++) {
-                System.out.print(" ");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                sb.append(" ");
             }
-            for(int j=0;j<(i+1)*2-1;j++) {
-                System.out.print("*");
+            for (int j = 0; j < i * 2 + 1; j++) {
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
 
-        for(int i=0;i<N-1;i++) {
-            for(int j=0;j<i+1;j++) {
-                System.out.print(" ");
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                sb.append(" ");
             }
-            for(int j=0;j<(2*N)-(2*(i+2)-1);j++) {
-                System.out.print("*");
+            for (int j = 0; j < N * 2 - (i + 1) * 2 - 1; j++) {
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
 
+        System.out.println(sb);
     }
 }
