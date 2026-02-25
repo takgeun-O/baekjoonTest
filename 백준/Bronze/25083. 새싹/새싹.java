11a1ba3656fbd8c47f26a62f90a341aced5d2d7c
takgeun-O <takgeun92@gmail.com>
2026-02-25 14:01:30 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,18 +1,22 @@
-
-
-import java.io.*;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("         ,r'\"7");
-        System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
+        StringBuilder sb = new StringBuilder();
+
+        sb
+                .append("         ,r'\"7").append("\n")
+                .append("r`-_   ,'  ,/").append("\n")
+                .append(" \\. \". L_r'").append("\n")
+                .append("   `~\\/").append("\n")
+                .append("      |").append("\n")
+                .append("      |");
+
+        System.out.println(sb);
     }
 }
