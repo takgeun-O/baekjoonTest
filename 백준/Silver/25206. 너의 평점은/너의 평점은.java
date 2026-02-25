42ffb03fa130b349f08d52520060237e79bc9d29
takgeun-O <takgeun92@gmail.com>
2026-02-25 15:18:31 +0900

    [Silver V] Title: 너의 평점은, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,53 +1,47 @@
-
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
-        // 전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값
 
-        // 전공과목의 과목명, 학점, 등급이 공백으로 구분되어 주어진다.
+        Map<String, Double> gradeMap = new HashMap<>();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, Double> map = new HashMap<>();
+        gradeMap.put("A+", 4.5);
+        gradeMap.put("A0", 4.0);
+        gradeMap.put("B+", 3.5);
+        gradeMap.put("B0", 3.0);
+        gradeMap.put("C+", 2.5);
+        gradeMap.put("C0", 2.0);
+        gradeMap.put("D+", 1.5);
+        gradeMap.put("D0", 1.0);
+        gradeMap.put("F", 0.0);
 
-        // 과목평점표 구성
-        map.put("A+", 4.5);
-        map.put("A0", 4.0);
-        map.put("B+", 3.5);
-        map.put("B0", 3.0);
-        map.put("C+", 2.5);
-        map.put("C0", 2.0);
-        map.put("D+", 1.5);
-        map.put("D0", 1.0);
-        map.put("F", 0.0);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        double sum1 = 0;    // (학점*과목평점)의 총합
-        double sum2 = 0;    // 학점의 총합
-        String grade;
-        double gradeScore = 0;
-        double credit;
+        double sum = 0.0;
+        double creditSum = 0.0;
+        for (int i = 0; i < 20; i++) {
+            String line = br.readLine();
+            StringTokenizer st = new StringTokenizer(line);
+            String subject = "";
+            double credit = 0;
+            String grade = "";
+            while (st.hasMoreTokens()) {
+                subject = st.nextToken();
+                credit = Double.parseDouble(st.nextToken());
+                grade = st.nextToken();
+            }
 
-        for(int i=0;i<20;i++) {
-            String input = reader.readLine();
-            String[] parts = input.split(" ");
-            grade = parts[2];
-            if(grade.equals("P")) {
+            if("P".equals(grade)) {
                 continue;
             }
-            credit = Double.parseDouble(parts[1]);
-            gradeScore = map.get(grade);
-//            System.out.println(credit + " " + grade + " " + gradeScore);
-            sum1 = sum1 + (credit*gradeScore);
-            sum2 = sum2 + credit;
+            sum = sum + credit * gradeMap.getOrDefault(grade, 0.0);
+            creditSum = creditSum + credit;
         }
 
-        System.out.println(sum1 / sum2);
-
+        System.out.println(sum / creditSum);
     }
 }
-
-
