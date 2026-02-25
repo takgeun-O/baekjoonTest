44cdb72b63c3ca4c7cf06a94ce8aeb86302a3087
takgeun-O <takgeun92@gmail.com>
2026-02-25 14:55:22 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,35 +1,33 @@
-
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
 
-        int flag = 0;
+        String line = br.readLine();
 
-        // 단어 길이 짝수일 때
-
-            for(int i=0;i<input.length()/2;i++) {
-//                System.out.println(input.charAt(i) + " vs " + input.charAt(input.length()-i-1));
-                if(input.charAt(i)!=input.charAt(input.length()-i-1)) {
-//                    System.out.println("진입");
-                    flag = 1;
+        int answer = 1;
+        if (line.length() % 2 == 0) {
+            for (int i = 0; i < line.length() / 2; i++) {
+                if (line.charAt(i) != line.charAt(line.length() - i - 1)) {
+                    answer = 0;
+                    break;
                 }
             }
-
-
-        if(flag==1) {
-            System.out.println(0);
         } else {
-            System.out.println(1);
+            for (int i = 0; i < line.length() / 2 + 1; i++) {
+                if (line.charAt(i) != line.charAt(line.length() - i - 1)) {
+                    answer = 0;
+                    break;
+                }
+            }
         }
+
+        System.out.println(answer);
     }
 }
