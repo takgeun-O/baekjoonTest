aba3fbd83f1703cb310d9390cd9e854bd660b9ba
takgeun-O <takgeun92@gmail.com>
2026-02-25 13:56:47 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,17 +1,23 @@
-
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        long a, b, c;
+    public static void main(String[] args) throws IOException {
 
-        a = scanner.nextLong();
-        b = scanner.nextLong();
-        c = scanner.nextLong();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
 
-        System.out.println(a + b + c);
+        long sum = 0;
+        if((line=br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            while(st.hasMoreTokens()) {
+                sum += Long.parseLong(st.nextToken());
+            }
+        }
+
+        System.out.println(sum);
     }
 }
