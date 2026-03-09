269ca5c65da6bab4d37446e125ff5564efeb2122
takgeun-O <takgeun92@gmail.com>
2026-03-09 15:31:50 +0900

    [Bronze II] Title: 수 정렬하기, Time: 164 ms, Memory: 16232 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int min = arr[i];
+            int index = i;
+            for (int j = i; j < N; j++) {
+                if(min > arr[j]) {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+
+        for(int i=0;i<N;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
